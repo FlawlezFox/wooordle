94671e7fe425589c8bac59183353bc5f00518508
FlawlezFox <armanarakelyan33@gmail.com>
2024-10-28 15:32:15 +0400

    fixed root component imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Root from "./components/Root";
-import { ThemeProvider } from "./components/Root/ThemeContext";
+import { ThemeProvider } from "src/components";
+import Root from "src/components/Root";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
